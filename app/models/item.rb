e64ae39751423b1f3a247d86e9e48754ac930d52
Myckel <m.uribe@portal53.cl>
2017-07-19 19:58:11 -0400

    sexto commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,11 @@ class Item < ApplicationRecord
   belongs_to :user, optional: true
   scope :itemsize, -> (size) { where(size: size)}
   scope :not_user, ->  { where(user: nil)}
+  after_destroy :last
+
+  def last
+      category.destroy unless category.items.any?
+  end
 
 end
 
